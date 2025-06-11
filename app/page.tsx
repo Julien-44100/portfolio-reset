ee0a8b6d3f8c078958ead865423937c19dbfffd0
Julien-44100 <julienbotar44100@yahoo.com>
2025-06-11 07:35:42 +0200

    work in

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,31 @@ export default function Home() {
 	return (
 		<div className={styles.page}>
 			{" "}
-			<h1>Bonjour</h1>
+			<h1 className={styles.slideInFromLeft}>
+				Je suis Julien Botar, développeur web junior ; découvrez mon
+				savoir-faire à travers ce portfolio.
+			</h1>
+			<section className={styles.frontendbackend}>
+				<section
+					className={`${styles.section} ${styles.slideInFromLeft} ${styles.frontend}`}
+				>
+					<h2>Front-End</h2>
+					<ul>
+						<li>HTML5</li>
+						<li>CSS3</li>
+						<li>JavaScript + React + Next.js</li>
+					</ul>
+				</section>
+				<section
+					className={`${styles.section} ${styles.slideInFromLeft} ${styles.backend}`}
+				>
+					<h2>Back-End</h2>
+					<ul>
+						<li>Express.js</li>
+						<li>SQL</li>
+					</ul>
+				</section>
+			</section>
 		</div>
 	);
 }
